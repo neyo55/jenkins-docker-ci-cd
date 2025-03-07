3db943b3a9964f5605626412833c9fb579f2a055
adeniyi rufai <kbneyo55@gmail.com>
2025-03-07 23:44:48 +0100

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD with Docker!"
+    return "Hello from Jenkins CI/CD with Docker!, task by neyo55"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
